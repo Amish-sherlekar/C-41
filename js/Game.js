ca464cfea8cb8169ac8737a13516ba307b31b532
Amish-sherlekar <asmitasherlekar@gmail.com>
2021-09-22 18:42:35 +0530

    a

--- js/Game.js
+++ js/Game.js
@@ -29,10 +29,29 @@ if(countRef.exists()){
 
     play (){
 
+        var newY=250
+
+        for( var plr in allPlayers){
+            if (plr === "player" + player.index) {
+                fill("red")
+            }
+            else {
+                fill("black")
+            }
+            text(allPlayers[plr].name+"; "+ allPlayers[plr].distance, 200, newY)
+            newY+=50
+        
+        }
+
         form.greetings.hide()
         textSize(30)
         text ("game start", 200, 200)
 
         player.getInfoPlayer()
+
+        if (keyDown(UP_ARROW)) {
+            player.distance += 50;
+            player.update()
+        }
     }
 }
